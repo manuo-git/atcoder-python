7054c532072e5e4303549be54a276680790842a8
manuo-git <saintmanuo@gmail.com>
2024-10-11 16:17:51 +0900

    snippetsを更新

--- gen_rand.py
+++ gen_rand.py
@@ -1,7 +1,10 @@
 from random import randint
 
-H, W = 5, 5
-N = randint(5, 10)
-print(H, W, N)
+N, X = randint(1, 10), randint(1, 1000)
+print(N, X)
 for _ in range(N):
-    print(randint(1, H), randint(1, W))
+    a = randint(1, 100)
+    b = randint(1, 100)
+    p = randint(1, 100)
+    q = randint(1, 100)
+    print(a, p, b, q)
