05a7c8a6218d0fba5afe57413562b7343b5e2a8a
manuo-git <saintmanuo@gmail.com>
2025-03-21 20:22:38 +0900

    コードファイルを追加

--- random_test/gen_rand.py
+++ random_test/gen_rand.py
@@ -1,6 +1,26 @@
 from random import *
 
-N = randint(1, 100)
-A = [randint(1, 10) for _ in range(N)]
-print(N)
-print(*A)
+# print(randint(1, 1000000))
+Q = randint(1, 1000)
+print(Q)
+S = set()
+M = (1<<30)-1
+# M = 10
+
+for _ in range(Q):
+    if len(S) > 0:
+        op = randint(0, 2)
+    else:
+        op = 0
+    if op == 0:
+        x = randint(0, M)
+        S.add(x)
+        print(op, x)
+    elif op == 1:
+        x = randint(0, M)
+        S.discard(x)
+        print(op, x)
+    else:
+        n = len(S)
+        x = randint(0, M)
+        print(op, randint(0, n-1), x)
