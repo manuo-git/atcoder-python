49da8dcecbb59bafc97fff9046f9dfe9f818a66b
manuo-git <saintmanuo@gmail.com>
2025-03-21 19:33:12 +0900

    dfs関数を検出するように処理を追加

--- lib/paste2clip.py
+++ lib/paste2clip.py
@@ -1,6 +1,32 @@
 import pyperclip
 
+saikikeisatu = [
+    " ■■■■■■■■■■■    ■ ■■■■■■  ■■■■■■■ ■          ■          ■      ■       ■ ",
+    "      ■         ■      ■    ■     ■■■■  ■■■■■■■■■■■     ■     ■■       ■ ",
+    "  ■■■■■■■■■  ■■ ■ ■■■■■■   ■■■■■■■■ ■   ■         ■  ■■■■■■■■          ■ ",
+    "  ■   ■   ■  ■■ ■ ■■■■■■   ■■■■ ■  ■■     ■■■■■■■■■    ■■              ■ ",
+    "  ■   ■   ■  ■■ ■           ■■■■■■■■■■   ■■ ■ ■  ■     ■               ■ ",
+    "  ■■■■■■■■■  ■■ ■■■■■■■■■    ■■■■■■■    ■■ ■   ■■      ■  ■■■■■        ■ ",
+    "  ■   ■   ■  ■■ ■■   ■   ■■■■■■■■■■■■■■  ■■■■■■■■■     ■  ■■■          ■ ",
+    "■■■■■■■■■■■■■  ■■■■■■■■■■   ■■■■■■■■■   ■■■■■■■■■■■   ■■               ■ ",
+    "  ■       ■    ■  ■  ■  ■   ■■■■■■■■■        ■  ■     ■                  ",
+    "  ■       ■    ■  ■  ■  ■   ■■■■■■■■■    ■■  ■   ■    ■   ■              ",
+    "  ■       ■   ■■  ■  ■ ■■   ■       ■   ■■   ■    ■  ■■   ■            ■ ",
+    "  ■     ■■■   ■      ■      ■■■■■■■■■      ■■■       ■    ■■■■■■       ■ ",
+]
+
+
+from typing import List
 with open("answer.py", "r", encoding="utf-8") as f:
-    lines = f.readlines()
+    lines: List[str] = f.readlines()
+    for line in lines:
+        if "dfs" in line:
+            for s in saikikeisatu:
+                print(s)
+            choice = input("\"dfs\"を検知しました。おまじないを書きましたか？ [y/N] :").lower()
+            if choice in ['y', 'ye', 'yes']:
+                break
+            elif choice in ['n', 'no']:
+                exit()
     text = "".join(lines)
     pyperclip.copy(text)
