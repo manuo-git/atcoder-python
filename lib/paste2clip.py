c8004adc47440a75044335000fcc2557861ee069
manuo-git <saintmanuo@gmail.com>
2025-11-09 09:11:20 +0900

    dfsを検知したときに"pypy"が含まれていたらスルーする

--- lib/paste2clip.py
+++ lib/paste2clip.py
@@ -19,14 +19,16 @@ saikikeisatu = [
 from typing import List
 with open("answer.py", "r", encoding="utf-8") as f:
     lines: List[str] = f.readlines()
+    dfsflag = False
+    pypyflag = False
     for line in lines:
-        if "dfs" in line:
-            for s in saikikeisatu:
-                print(s)
-            choice = input("\"dfs\"を検知しました。おまじないを書きましたか？ [y/N] :").lower()
-            if choice in ['y', 'ye', 'yes']:
-                break
-            elif choice in ['n', 'no']:
-                exit()
+        if "dfs" in line: dfsflag = True
+        if "pypy" in line: pypyflag = True
+
+    if dfsflag and not pypyflag:
+        for s in saikikeisatu:
+            print(s)
+        print("\"dfs\"を検知しました。おまじないが必要かも。")
+        exit()
     text = "".join(lines)
     pyperclip.copy(text)
