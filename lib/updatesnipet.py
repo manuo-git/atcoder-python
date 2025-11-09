6165e1b05cbcc3f3d417b8121652698bb14a935b
manuo-git <saintmanuo@gmail.com>
2025-11-09 09:12:28 +0900

    作業HDDのドライブ名を変更

--- lib/updatesnipet.py
+++ lib/updatesnipet.py
@@ -3,5 +3,5 @@ lines = []
 with open("C:\\Users\\manuo\\AppData\\Roaming\\Code\\User\\snippets\\python.json", encoding="utf-8") as f:
     lines = f.readlines()
 
-with open("M:\\PythonScripts\\atcoder\\python\\snippets.json", "w", encoding="utf-8") as f:
+with open("D:\\PythonScripts\\atcoder\\python\\snippets.json", "w", encoding="utf-8") as f:
     f.writelines(lines)
